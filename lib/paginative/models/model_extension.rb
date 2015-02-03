cae8fcee4e2c61c6c1af730ff0f7449003545fff
Isaac Norman <isaacdnorman@gmail.com>
2015-02-03 18:15:51 +1100

    Bumped version to 0.0.18 and removed the unecessary quotes

--- lib/paginative/models/model_extension.rb
+++ lib/paginative/models/model_extension.rb
@@ -23,8 +23,8 @@ module Paginative
         def self.with_field_from(field="", value="", limit=25, order="asc")
           field = "#{self.table_name}.#{field}"
 
-          return self.order("sanitized_ordering(field, order)").where("#{field} < ?", value).limit(limit) if order.downcase == "desc"
-          self.order("sanitized_ordering(field, order)").where("#{field} > ?", value).limit(limit)
+          return self.order(sanitized_ordering(field, order)).where("#{field} < ?", value).limit(limit) if order.downcase == "desc"
+          self.order(sanitized_ordering(field, order)).where("#{field} > ?", value).limit(limit)
         end
     end
   end
