4a6c7a377a27a314887abbfed4fcf15faa3d3ba0
Isaac Norman <isaacdnorman@gmail.com>
2015-04-15 17:56:19 +1000

    Forgot to include the direction in the ' marks for sorting

--- lib/paginative/models/ordering_helpers.rb
+++ lib/paginative/models/ordering_helpers.rb
@@ -5,7 +5,7 @@ module Paginative
     def sanitized_ordering(table_name, field, order)
       if field.is_a? Array
         return raise "Wrong number of sorting fields. Expected 2, got #{field.length}. If you want to sort by a singular field please pass field argument as a string rather than an array." unless field.length == 2
-        "'#{table_name}.#{sanitize_column(field[0])} || #{table_name}.#{sanitize_column(field[1])}' #{sanitize_column_direction(order)}"
+        "'#{table_name}.#{sanitize_column(field[0])} || #{table_name}.#{sanitize_column(field[1])} #{sanitize_column_direction(order)}'"
       else
         "#{table_name}.#{sanitize_column(field)} #{sanitize_column_direction(order)}"
       end
