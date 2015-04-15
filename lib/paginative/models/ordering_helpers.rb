af2a3d4f2be20fbff92208a4c16d418f4e814441
Isaac Norman <isaacdnorman@gmail.com>
2015-04-15 17:51:47 +1000

    Adding the new ordering helpers

--- lib/paginative/models/ordering_helpers.rb
+++ lib/paginative/models/ordering_helpers.rb
@@ -3,7 +3,12 @@ module Paginative
     extend ActiveSupport::Concern
 
     def sanitized_ordering(table_name, field, order)
-      "#{table_name}.#{sanitize_column(field)} #{sanitize_column_direction(order)}"
+      if field.is_a? Array
+        return raise "Wrong number of sorting fields. Expected 2, got #{field.length}. If you want to sort by a singular field please pass field argument as a string rather than an array." unless field.length == 2
+        "'#{table_name}.#{sanitize_column(field[0])} || #{table_name}.#{sanitize_column(field[1])}' #{sanitize_column_direction(order)}"
+      else
+        "#{table_name}.#{sanitize_column(field)} #{sanitize_column_direction(order)}"
+      end
     end
 
     private
