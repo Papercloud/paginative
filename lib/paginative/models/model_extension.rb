008f97d46322b476d641569ef9320cfa338d22b1
Isaac Norman <isaacdnorman@gmail.com>
2015-04-15 16:18:44 +1000

    Updated the gem to allow the passing of more than one sort field

--- lib/paginative/models/model_extension.rb
+++ lib/paginative/models/model_extension.rb
@@ -21,8 +21,18 @@ module Paginative
         end
 
         def self.with_field_from(field="", value="", limit=25, order="asc")
-          return self.order(sanitized_ordering(self.table_name, field, order)).where("#{field} < ?", value).limit(limit) if order.downcase == "desc"
-          self.order(sanitized_ordering(self.table_name, field, order)).where("#{field} > ?", value).limit(limit)
+          if field.is_a? Array
+            return raise "Wrong number of sorting fields. Expected 2, got #{field.length}. If you want to sort by a singular field please pass field argument as a string rather than an array." unless field.length == 2
+            # You can now pass in an array of 'field' params so that you can have a secondary sort order.
+            # This is important if your primary sort field could have duplicate values
+            primary_sort_field = field[0]
+            secondary_sort_field = field[1]
+            return self.order(sanitized_ordering(self.table_name, primary_sort_field, order), sanitized_ordering(self.table_name, secondary_sort_field, order)).where("#{secondary_sort_field} < ?", value).limit(limit) if order.downcase == "desc"
+            self.order(sanitized_ordering(self.table_name, primary_sort_field, order), sanitized_ordering(self.table_name, secondary_sort_field, order)).where("#{secondary_sort_field} > ?", value).limit(limit)
+          else
+            return self.order(sanitized_ordering(self.table_name, field, order)).where("#{field} < ?", value).limit(limit) if order.downcase == "desc"
+            self.order(sanitized_ordering(self.table_name, field, order)).where("#{field} > ?", value).limit(limit)
+          end
         end
     end
   end
