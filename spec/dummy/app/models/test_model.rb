8a4585defba1b3eff1757b82e93ae5cd20d85e43
Isaac Norman <isaacdnorman@gmail.com>
2015-04-16 14:27:23 +1000

    Removed redundant call that was geocoding test_models in the specs. We don't want that

--- spec/dummy/app/models/test_model.rb
+++ spec/dummy/app/models/test_model.rb
@@ -2,5 +2,4 @@ class TestModel < ActiveRecord::Base
   include Paginative::ModelExtension
 
   reverse_geocoded_by :latitude, :longitude
-  # after_validation :reverse_geocode          # auto-fetch coordinates
 end
