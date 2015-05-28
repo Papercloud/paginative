1d0f8ed127613b3d13d135e0b7a79f28f9c2b1dc
William Porter <dawilster143@gmail.com>
2015-05-28 15:04:35 +1000

    When starting foreman because the test_model included geo_coder it could potientally conflict with another gem that includes modules to ActiveRecord::Base so I moved it to a spec class_eval so the app user can include on a case by case basis.

--- app/models/paginative/test_model.rb
+++ app/models/paginative/test_model.rb
@@ -1,8 +1,5 @@
 module Paginative
   class TestModel < ActiveRecord::Base
-    include Paginative::ModelExtension
 
-    reverse_geocoded_by :latitude, :longitude
-    after_validation :reverse_geocode          # auto-fetch coordinates
   end
 end
